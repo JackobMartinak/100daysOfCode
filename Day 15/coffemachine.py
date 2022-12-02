e0f9a1eabe94f5d7a15ff0e176f1b83057b33e24
unknown <jakub.martinak@outlook.com>
2022-12-02 16:01:24 +0100

    finished day 15/ Coffee machine

--- Day 15/coffemachine.py
+++ Day 15/coffemachine.py
@@ -31,9 +31,61 @@ resources = {
     "money": 0,
 }
 
-print(f"{resources.items()}")
-# def check_status(res):
-#     print(f"{res[0]}: {res.get(0)}")
+
+def check_status(res):
+    for key, value in res.items():
+        # print(key.capitalize(), ": ", key == "water" or key == "milk" ? value + "ml": value)
+        print(
+            key.capitalize(), ": ", str(value) + "ml"
+        ) if key == "water" or key == "milk" else print(key.capitalize(), ": ", value)
+    print("\n")
+
+
+def calculate_resources(res, menu, u_input):
+    """Will calculate the remaining resources after making a coffe and save them to the
+    dictonary"""
+    for key, value in menu[u_input]["ingredients"].items():
+        if res.get(key) >= value:
+            quarters = float(input("How many quarters?: ")) * 0.25
+            dimes = float(input("How many dimes?: ")) * 0.10
+            nickles = float(input("How many nickles?: ")) * 0.05
+            pennies = float(input("How many pennies?: ")) * 0.01
+            money = quarters + dimes + nickles + pennies
+            if money > menu[u_input]["cost"]:
+                res["money"] = res["money"] + menu[u_input]["cost"]
+                change = money - menu[u_input]["cost"]
+                print(f"Here is your {u_input}")
+                print(f"Here is your change ${change}")
+                for keys, values in menu[u_input]["ingredients"].items():
+                    res[keys] = res.get(keys) - values
+                break
+            else:
+                print("Not enough money.")
+                break
+
+        else:
+            print(f"Insufficient resources. You don't have enough {key}")
+            break
+
 
 while True:
-    break
+    user_input = input(
+        "What would you like? (espresso/latte/cappuccino/report/exit): "
+    ).lower()
+    if user_input == "report":
+        check_status(resources)
+        continue
+    elif user_input == "espresso":
+        calculate_resources(resources, MENU, user_input)
+        continue
+    elif user_input == "latte":
+        calculate_resources(resources, MENU, user_input)
+        continue
+    elif user_input == "cappuccino":
+        calculate_resources(resources, MENU, user_input)
+        continue
+    elif user_input == "exit":
+        break
+    else:
+        print("===== Unkown command =====")
+        continue
