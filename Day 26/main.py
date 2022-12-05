e7b8a79fd370c0c5261fe102dc15c54c9ce1cf12
unknown <jakub.martinak@outlook.com>
2022-12-05 18:45:46 +0100

    Day 26

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,2 +1,17 @@
-with open("./text.txt") as p:
-    print(p.read())
+# with open(
+#     "C:\\Users\\Repik\\OneDrive - Slovenská technická univerzita v Bratislave\\Python 100 days\\Day 26\\text.txt"
+# ) as p:
+# print(p.read())
+
+# LIST COMPREHENSION
+numbers = [1, 2, 3]
+new_numbers = [n * 2 for n in numbers]
+print(numbers)
+print(new_numbers)
+
+name = "Jackob"
+letters_list = [letter for letter in name]
+print(letters_list)
+
+range_list = [num * 2 for num in range(1, 5)]
+print(range_list)
